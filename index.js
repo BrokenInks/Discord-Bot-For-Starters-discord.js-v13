1c9e9d843cc2d9b57adef1d3c4a1ce0b5a64ac8f
BrokenInk <69753561+BrokenInks@users.noreply.github.com>
2023-02-05 18:44:34 +0300

    ыы

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_PRESENCES]
 });
 
+
 client.commands = new Collection();
 client.aliases = new Collection();
 
